0ecbc68946fdc1d9c10905c2d7b148d022a9f0a4
Flavio Rogerio <solebac@hotmail.com>
2022-07-12 00:47:45 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
   return (
     <>
       <h1>Olá Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
     </>
   );
 }
